a55e876f853cd08cc4287ccf2e1422866528bc4f
obaraujo <viniciusaraujotech@gmail.com>
2023-09-05 08:55:37 -0300

    bug resize popup

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -17,7 +17,6 @@ export function Popup({
   const popup = useRef(null);
 
   function handleStop(e, data: DraggableData) {
-    document.body.style.overflowY = "auto";
     const heightWindow = window.innerHeight;
 
     if (heightWindow <= heightPopup / 2 + data.y) {
@@ -32,18 +31,19 @@ export function Popup({
     }
   }
 
-  function handleStart() {
-    document.body.style.overflowY = "hidden";
-  }
-
   useEffect(() => {
-    const heightPopup = popup.current.getBoundingClientRect().height;
+    document.body.style.overflowY = "hidden";
+
     const heightWindow = window.innerHeight;
 
     setPositionY(heightWindow - heightPopup);
     setPositionBoundY(heightWindow - heightPopup - 5);
     setHeightPopup(heightPopup);
-  }, [children]);
+
+    return () => {
+      document.body.style.overflowY = "auto";
+    };
+  }, [popup.current, heightPopup]);
 
   return (
     <Portal.Root>
@@ -59,18 +59,20 @@ export function Popup({
         grid={[1, 1]}
         scale={1}
         onStop={handleStop}
-        onStart={handleStart}
         bounds={{ top: positionBoundY }}
       >
         <main
           ref={popup}
+          onLoad={(e) => setHeightPopup(e.currentTarget.clientHeight)}
           style={{
             transition: "all 0.25s",
-            height: heightPopup ? heightPopup : null,
+            // height: heightPopup ? heightPopup : null,
           }}
           className={`fixed p-4 top-0 left-0 right-0 z-50  bg-white rounded-tr-2xl rounded-tl-2xl border-t border-orange-barapi`}
         >
-          <div className="handle bg-gray-400 w-20 h-3 mx-auto rounded-full mb-4 hover:bg-gray-300 transition-all cursor-pointer"></div>
+          <div className="handle w-full h-8">
+            <div className="bg-gray-400 w-20 h-3 mx-auto rounded-full hover:bg-gray-300 transition-all cursor-pointer"></div>
+          </div>
           {children}
         </main>
       </Draggable>
