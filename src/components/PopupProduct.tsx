fb5ecc0082913eae2edda662a8f88114ea09d9af
obaraujo <viniciusaraujotech@gmail.com>
2023-09-05 09:57:16 -0300

    buttons in popup

--- src/components/PopupProduct.tsx
+++ src/components/PopupProduct.tsx
@@ -1,11 +1,16 @@
 import { imageProps } from "@/types/images";
 import { usePopupProduct } from "contexts/popupProduct";
+import { useScanner } from "contexts/scanner";
 import { getInfoQuantity } from "functions";
+import { useState } from "react";
+import { BiScan } from "react-icons/bi";
+import { BsChatTextFill } from "react-icons/bs";
 import { useQuery } from "react-query";
 import { apiBarapiV2 } from "services/api";
 import { CardProductPrice } from "./CardProductPrice";
 import { Gallery } from "./Gallery";
 import { Popup } from "./Popup";
+import { PopupChat } from "./PopupChat";
 import { SkeletonPopupProduct } from "./Skeleton";
 
 interface ProductProps {
@@ -23,12 +28,14 @@ interface ProductProps {
   gallery: imageProps[];
   conference: {
     quantity: string;
-    status: "revision";
+    status: "revision" | "complete";
   };
 }
 
 export function PopupProduct({ productId }: { productId: number }) {
   const { setProductId } = usePopupProduct();
+  const [openChat, setOpenChat] = useState(false);
+  const { setProductFetched, setActiveScanner } = useScanner();
 
   function handleClose() {
     setProductId(0);
@@ -50,39 +57,70 @@ export function PopupProduct({ productId }: { productId: number }) {
   );
 
   return (
-    <Popup onClose={handleClose}>
-      {!product ? (
-        <SkeletonPopupProduct />
-      ) : (
-        <main className="scrollbar-hidden md:scrollbar-show max-h-[100%] overflow-y-auto pb-1">
-          <Gallery images={[product.image, ...product.gallery]} />
-          <span className="font-bold text-4xl text-orange-barapi">
-            {product.quantity}x
-          </span>
-          <h3 className="font-semibold text-base capitalize">{product.name}</h3>
-          <CardProductPrice
-            price={product.price}
-            regular={product.regular_price}
-          />
-
-          <div className="mt-4 text-gray-500">
-            <div>
-              <strong className="font-bold">Código de barras: </strong>
-              <span className="font-semibold">{product.bar_code}</span>
-            </div>
-            <div>
-              <strong className="font-bold">Conteúdo: </strong>
-              <span className="font-semibold">
-                {getInfoQuantity(product.name)}
+    <>
+      <Popup onClose={handleClose} fullscreen>
+        {!product ? (
+          <SkeletonPopupProduct />
+        ) : (
+          <main className="scrollbar-hidden md:scrollbar-show max-h-[100%] overflow-y-auto pb-1">
+            <div className="handle">
+              <Gallery images={[product.image, ...product.gallery]} />
+              <span className="font-bold text-4xl text-orange-barapi">
+                {product.quantity}x
               </span>
+              <h3 className="font-semibold text-base capitalize">
+                {product.name}
+              </h3>
+              <CardProductPrice
+                price={product.price}
+                regular={product.regular_price}
+              />
+
+              <div className="mt-4 text-gray-500">
+                <div>
+                  <strong className="font-bold">Código de barras: </strong>
+                  <span className="font-semibold">{product.bar_code}</span>
+                </div>
+                <div>
+                  <strong className="font-bold">Conteúdo: </strong>
+                  <span className="font-semibold">
+                    {getInfoQuantity(product.name)}
+                  </span>
+                </div>
+                <div>
+                  <strong className="font-bold">Estoque: </strong>
+                  <span className="font-semibold">{product.stock}</span>
+                </div>
+              </div>
             </div>
-            <div>
-              <strong className="font-bold">Estoque: </strong>
-              <span className="font-semibold">{product.stock}</span>
+            <div className="flex gap-2 mt-4">
+              <button
+                onClick={() => setOpenChat(true)}
+                className="flex gap-1 items-center border-orange-barapi border  rounded-lg flex-1 text-orange-barapi font-bold px-6 py-3 justify-center disabled:bg-gray-400"
+              >
+                <BsChatTextFill /> Chat
+              </button>
+              {product.conference.status !== "complete" && (
+                <button
+                  onClick={() => {
+                    setProductFetched(product);
+                    setActiveScanner(true);
+                  }}
+                  className="flex gap-1 items-center bg-orange-barapi rounded-lg flex-1 text-white font-bold px-6 py-3 justify-center disabled:bg-gray-400"
+                >
+                  <BiScan />
+                  Escanear
+                </button>
+              )}
             </div>
-          </div>
-        </main>
+          </main>
+        )}
+      </Popup>
+      {openChat && (
+        <Popup onClose={() => setOpenChat(false)}>
+          <PopupChat onSendMessage={() => setOpenChat(false)} />
+        </Popup>
       )}
-    </Popup>
+    </>
   );
 }
